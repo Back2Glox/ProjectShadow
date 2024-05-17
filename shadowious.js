ab0154f127eb18844d91107196bfd2e9b9719da6
Glox <106978805+Back2Glox@users.noreply.github.com>
2024-05-17 15:45:14 +0800

    Update shadowious.js

--- shadowious.js
+++ shadowious.js
@@ -1,7 +1,11 @@
+const correctPassword = '0uHS0x58Mg9sN&rCwwCm**EPC2M@$sHu9^&fh35qNG3b!dauSWtwMM877KTBvgv0'; // Change this to your actual password
+
 function checkPassword() {
     const password = document.getElementById('password').value;
-    if (password === '0uHS0x58Mg9sN&rCwwCm**EPC2M@$sHu9^&fh35qNG3b!dauSWtwMM877KTBvgv0') {
+    if (password === correctPassword) {
         document.getElementById('lore').style.display = 'block';
+        document.getElementById('password').style.display = 'none';
+        document.querySelector('button').style.display = 'none';
     } else {
         alert('Incorrect password. Please try again.');
     }
